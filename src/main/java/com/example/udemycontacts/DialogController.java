acaa4ab169eb9d2feb4f550d7715c9a8c47e4234
Skoomatron <trevor.jeffrey.edwards@gmail.com>
2022-10-27 15:09:03 -0400

    Add Contact Dialog Box

---
+++ src/main/java/com/example/udemycontacts/DialogController.java
@@ -0,0 +1,17 @@
+package com.example.udemycontacts;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class DialogController {
+    @FXML
+    private TextField firstName;
+    @FXML
+    private TextField lastName;
+    @FXML
+    private TextField phoneNumber;
+    @FXML
+    private TextArea notes;
+
+}
--- src/main/java/com/example/udemycontacts/MainController.java
+++ src/main/java/com/example/udemycontacts/MainController.java
@@ -1,21 +1,49 @@
 package com.example.udemycontacts;
 
 import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class MainController {
-//    @FXML
-//    private TableView<ContactData> tableView;
-//    @FXML
-//    private PropertyValueFactory firstName;
-//    @FXML
-//    private PropertyValueFactory lastName;
-//    @FXML
-//    private PropertyValueFactory phoneNumber;
-//    @FXML
-//    private PropertyValueFactory notes;
+    @FXML
+    private BorderPane mainBorderPane;
 
-
+    @FXML
     public void exitApplication() {
         Platform.exit();
+    }
+    @FXML
+    public void addContact() {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainBorderPane.getScene().getWindow());
+        dialog.setTitle("Add New Contact");
+        dialog.setHeaderText("Contact");
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("contactDialog.fxml"));
+
+        try {
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+        } catch (IOException e) {
+            System.out.println("Couldn't Find the Dialog");
+            e.printStackTrace();
+            return;
+        }
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+        Optional<ButtonType> result = dialog.showAndWait();
+
+//        if (result.isPresent() && result.get() == ButtonType.OK) {
+//            DialogController controller = fxmlLoader.getController();
+//            TodoItem newItem = controller.processResults();
+//            todoListView.getSelectionModel().select(newItem);
+//        }
     }
 }
